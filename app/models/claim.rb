b58a4bff804e90b4ebc2bfc2ca0d56e80a7c3e3a
Isaac Weinbach <isaacito@aol.com>
2015-01-10 22:21:56 -0500

    form for file upload, and validation

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,5 +1,6 @@
 class Claim < ActiveRecord::Base
 	belongs_to :user
+    validates :upload, :attachment_presence => true
 
     has_attached_file :upload, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
