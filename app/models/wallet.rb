d35340256c5826ded4d000204d0f76d074810b06
Anuvis Mejia <ameji012@gmail.com>
2015-01-10 20:29:28 -0500

    adding methods to wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -14,6 +14,33 @@ class Wallet < ActiveRecord::Base
 
   end
 
+  def create_key_pairs
+    #gernerating key pairs
+    keypair = EncryptoSigno.generate_keypair
+    private_key = keypair.to_s
+    public_key  = keypair.public_key.to_s
+
+    #signing of private key
+    string = SecureRandom.base64
+    signature = EncryptoSigno.sign(private_key, string)
+
+    key = signature
+    key_join = key.split.join
+    key_by_2 = (key.split.join.size / 2)
+
+    user1_key = key.split.join[0..key_by_2]
+    user2_key = key.split.join[(key_by_2 + 1)..key_join.size]
+
+    #after user is deseased a claim key is used to unlock account
+    claim_key = user1_key + user2_key
+    claim_key2 = user2_key + user1_key
+
+    
+  end
+
+  # def destroy_key_pairs
+  # end
+
 
 
 end
