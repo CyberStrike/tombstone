e2936f61639729aef18456346f194d88b3965ee6
Mark Natera <marknatera@gmail.com>
2015-01-10 23:40:31 -0500

    Changed inside page path to wallet path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Tombstone::Application.configure do
   config.assets.debug = true
   
   # Mailer
-   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tombstone::Application.routes.draw do
 
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
-  get "inside", to: "pages#inside", as: "inside"
+  get "wallet", to: "pages#wallet", as: "wallet"
   
       
   devise_for :users
