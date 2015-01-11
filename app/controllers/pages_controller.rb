b0105431aab1a251815c4f27e0ac0b01f9821600
Chris Scott <cyberstrike@gmail.com>
2015-01-11 13:11:49 -0500

    generate keys page begun

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,24 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:inside]
+  before_action :authenticate_user!, only: [:inside, :create_keys]
 
   def home
   end
   
   def inside
   end
-   
-    
+
+  def get_keys
+  end
+
+
+  def create_keys
+    respond_to do |format|
+      if current_user.create_key_pairs
+        format.html { redirect_to keys_path, notice: 'Keys was successfully created.' }
+      else
+        format.html { redirect_to wallets_path, alert: 'Keys failed to be created.' }
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Tombstone::Application.routes.draw do
 
   root to: "pages#home"
   get "home", to: "pages#home", as: "home"
-  get "wallet", to: "pages#wallet", as: "wallet"
-      
+  # get "wallet", to: "pages#wallet", as: "wallet"
+  get "get_keys", to: "pages#get_keys", as: "keys"
+  post "create_keys", to: "pages#create_keys"
+
   devise_for :users
   
   namespace :admin do
